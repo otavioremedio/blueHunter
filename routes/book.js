fee8cd7afc97cd5a616ddf1c00c4b0fea5470991
otavio.remedio <otavio.remedio@GTI578699.senac.corp>
2017-11-24 08:56:39 -0200

    root

---
+++ routes/book.js
@@ -0,0 +1,13 @@
+
+/*
+ * GET users listing.
+ */
+exports.list_by_title = function(req, res){
+  res.send("respond with a title");
+};
+
+exports.list_by_author = function(req, res){
+	  res.send("respond with a title");
+	};
+
+
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+
+/*
+ * GET users listing.
+ */
+
+exports.list = function(req, res){
+  res.send("respond with a resource");
+};
+
+exports.list_by_name = function(req, res){
+  res.send("respond with a name");
+};
+
