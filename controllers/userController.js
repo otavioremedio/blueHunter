adebbbf7f1a3621240b4fc83d6dab1f7853e7f33
otavioremedio <otavioremedio@gmail.com>
2017-11-28 09:01:14 -0200

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ var user = require('../models/User');
 
 exports.list_by_name = function(req, res){
 
-  user.find({'name': new RegExp(req.query.value, 'i')}, function(err, users){
+  user.find({}, function(err, users){
     if(err){
       console.log(err);
     } else {
